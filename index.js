470e3b8917d984683dda7cf622f04f17a9889aa1
joojdev <jvpiovezan2905@gmail.com>
2022-05-29 18:36:54 -0300

    New feature of rooms

--- index.js
+++ index.js
@@ -12,33 +12,58 @@ let userArray = []
 
 expressApp.use(express.static(path.join(__dirname, folder)))
 
-expressApp.get('/api/user_counter', (request, response) => {
-  response.status(200).json({ online: userArray.length })
+expressApp.get('/api/user_counter/:roomQuery', (request, response) => {
+  const { roomQuery } = request.params
+  
+  if (!roomQuery) return response.status(404).end()
+
+  const userCount = userArray.filter((user) => user.room == roomQuery).length
+
+  response.status(200).json({ online: userCount })
+})
+
+expressApp.get('/api/get_current_rooms', (request, response) => {
+  const roomArray = userArray.map((user) => user.room)
+
+  if (!roomArray) return response.status(404).end()
+
+  const roomCounter = {}
+
+  roomArray.forEach((roomName) => {
+    roomCounter[roomName] = roomCounter[roomName] + 1 || 1
+  })
+
+  response.status(200).json(roomCounter)
 })
 
 websocketServer.on('connection', (websocketClient) => {
 	console.log(`A user connected! ID: ${websocketClient.id}`)
 
-  websocketClient.on('newUser', ({ username }) => {
-    userArray.push({ id: websocketClient.id, username })
+  websocketClient.on('newUser', ({ username, room }) => {
+    if (!username || !room) return websocketClient.disconnect()
+    if (username.length > 30 || room.length > 15) return websocketClient.disconnect()
+
+    websocketClient.join(room)
+    userArray.push({ id: websocketClient.id, username, room })
     websocketClient.emit('userConfirm')
-    websocketServer.emit('userAdded', { username })
+    websocketServer.to(room).emit('userAdded', { username })
     console.log(userArray)
   })
 
   websocketClient.on('messageAdd', ({ message }) => {
-    const { username } = userArray.find((user) => user.id == websocketClient.id)
+    const { username, room } = userArray.find((user) => user.id == websocketClient.id)
 
-    websocketServer.emit('messageAdded', { username, message })
+    websocketServer.to(room).emit('messageAdded', { username, message })
   })
 
   websocketClient.on('disconnect', () => {
     console.log(`A user disconnected! ID: ${websocketClient.id}`)
+    console.log(userArray)
 
     const userIndex = userArray.findIndex((user) => user.id == websocketClient.id)
     if (userIndex == -1) return
-    const { username } = userArray[userIndex]
-    websocketServer.emit('userRemoved', { username })
+    const { username, room } = userArray[userIndex]
+    websocketServer.to(room).emit('userRemoved', { username })
 
     userArray.splice(userIndex, 1)
   })
---
+++ public/chat/main.js
@@ -0,0 +1,102 @@
+const messageList = document.querySelector('#messageList')
+const inputContainer = document.querySelector('#inputContainer')
+const messageInput = document.querySelector('#messageInput')
+const chatContainer = document.querySelector('#chatContainer')
+const userCounter = document.querySelector('#userCounter')
+const title = document.querySelector('#title')
+
+const websocketClient = io()
+let connected = false
+
+async function updateUserCounter() {
+  const response = await fetch(`/api/user_counter/${sessionStorage.room}`)
+  const data = await response.json()
+
+  userCounter.textContent = `${data.online} users online!`
+}
+
+function start() {
+  if (!sessionStorage.username) return window.location.href = '/'
+  const username = sessionStorage.username.trim().split(/ +/).join` `
+
+  if (!sessionStorage.room) return window.location.href = '/'
+  const room = sessionStorage.room.trim().split(/ +/).join` `
+  
+  if (username.length > 30) return window.location.href = '/'
+  if (room.length > 15) return window.location.href = '/'
+
+  websocketClient.emit('newUser', {
+    username,
+    room
+  })
+}
+
+websocketClient.on('userConfirm', () => {
+  connected = true
+  title.textContent = `Chat App - ${sessionStorage.room}`
+})
+
+function sendSystemMessage(message) {
+  const systemMessageElement = document.createElement('li')
+  systemMessageElement.className = 'systemMessage'
+  systemMessageElement.textContent = message
+  messageList.append(systemMessageElement)
+  
+  chatContainer.scrollTop = chatContainer.scrollHeight
+}
+
+websocketClient.on('userAdded', ({ username }) => {
+  if (!connected) return window.location.href = '/'
+  console.log(username)
+  
+  sendSystemMessage(`${username} entered the chat!`)
+  updateUserCounter()
+})
+
+websocketClient.on('userRemoved', ({ username }) => {
+  if (!connected) return window.location.href = '/'
+  
+  sendSystemMessage(`${username} left the chat!`)
+  updateUserCounter()
+})
+
+websocketClient.on('messageAdded', ({ username, message }) => {
+  if (!connected) return window.location.href = '/'
+  
+  console.log(username, message)
+  const messageBoxElement = document.createElement('li')
+  messageBoxElement.className = 'messageBox'
+
+  const usernameElement = document.createElement('p')
+  usernameElement.className = 'username'
+  usernameElement.textContent = username
+  
+  const messageElement = document.createElement('p')
+  messageElement.className = 'message'
+  messageElement.textContent = message
+  
+  messageBoxElement.append(usernameElement)
+  messageBoxElement.append(messageElement)
+  messageList.append(messageBoxElement)
+
+  chatContainer.scrollTop = chatContainer.scrollHeight
+})
+
+inputContainer.addEventListener('submit', (event) => {
+  event.preventDefault()
+  if (!connected) return window.location.href = '/'
+  
+  const message = messageInput.value.trim()
+  
+  if (message.length > 150) return
+  if (!message) return
+  
+  messageInput.value = ''
+  websocketClient.emit('messageAdd', ({ message }))
+})
+
+websocketClient.on('disconnect', () => {
+  window.location.reload()
+})
+
+start()
